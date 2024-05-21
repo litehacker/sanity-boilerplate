ba40f04dcbfc9ebdea6823e60b55b886d98c69e1
Giorgi Gvimradze <natceo@gmail.com>
2024-05-21 16:27:51 +0400

    two datasets

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,20 +2,31 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {RobotIcon, RocketIcon} from '@sanity/icons'
 
-export default defineConfig({
-  name: 'default',
-  title: 'sanity-boilerplate',
-
-  projectId: 'ao2ae4ct',
-  dataset: 'production',
-
-  plugins: [
-    structureTool(),
-    visionTool(),
-  ],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    projectId: 'ao2ae4ct',
+    dataset: 'production',
+    name: 'production-workspace',
+    basePath: '/production',
+    title: 'Production workspace',
+    icon: RobotIcon,
+    plugins: [structureTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    projectId: 'ao2ae4ct',
+    dataset: 'staging',
+    name: 'staging-workspace',
+    basePath: '/staging',
+    title: 'Staging Workspace',
+    icon: RocketIcon,
+    plugins: [structureTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
