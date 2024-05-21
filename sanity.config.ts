83ab5d8fc07cbb1c1412b7fc7c2abe9cb0d07663
Giorgi Gvimradze <natceo@gmail.com>
2024-05-21 14:12:14 +0400

    initial setup

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanity-boilerplate',
+
+  projectId: 'fgga37ss',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
